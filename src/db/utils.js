90b9b4a37d93570422f54d48a1fc76819adfa113
f0rc <80628768+f0rc@users.noreply.github.com>
2024-05-22 11:35:31 -0500

    oops

--- src/db/utils.js
+++
@@ -1,5 +0,0 @@
-export const sendUnauthorizedResponse = (res) => {
-  res.statusCode = 401;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ message: "NOT FOUND", code: "UNAUTHORIZED" }));
-};
